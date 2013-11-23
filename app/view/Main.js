96b744e13d976965897f8f9d11b3979217e55c73
Joe <joeciardelli@gmail.com>
2013-11-23 17:57:40 -0600

    fixed missing Main.js

---
+++ app/view/Main.js
@@ -0,0 +1,60 @@
+Ext.define('tleaf.view.Main', {
+	extend: 'Ext.tab.Panel',
+	    xtype: 'main',
+	    requires: [
+		       'Ext.TitleBar',
+        'Ext.Video'
+		       ],
+	    config: {
+	    tabBarPosition: 'bottom',
+
+		items: [
+			{
+			    title: 'Home',
+				iconCls: 'home',
+
+				styleHtmlContent: true,
+				scrollable: true,
+
+				items: {
+				docked: 'top',
+				    xtype: 'titlebar',
+
+				    title: 'Welcome to t.leaf'
+
+
+				    },
+
+				html: [
+                    "Welcome to t.leaf"
+				       ].join("")
+				},
+			{
+			    title: 'Teas',
+				iconCls: 'action',
+
+				items: [
+					{
+					    docked: 'top',
+						xtype: 'titlebar',
+						title: 'Getting Started'
+						},
+					{
+					    xtype: 'video',
+						url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
+						posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+						}
+                ]
+				},
+			{
+			    title: 'Blender',
+				iconCls: 'action'
+				},
+			{
+			    title: 'Info',
+				iconCls: 'action'
+
+				}
+        ]
+		}
+    });
